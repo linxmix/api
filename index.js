ee03843f84a84ce36b26c32ff89b4c5d7dad13f0
Michael Williams <dinosaur@riseup.net>
2014-06-23 03:21:00 -0700

    remove extra space

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var port = process.env.PORT || 5000;
 
 app.listen(port);
 
-console.log("Linx API server listening on port ", port);
+console.log("Linx API server listening on port", port);
 
 module.exports = app;
